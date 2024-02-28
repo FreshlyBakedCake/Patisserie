a1b22b87b658af4ff28923044d7b2ab45c281178
Samuel Shuert <coded@clicks.codes>
2024-02-28 17:43:13 -0500

    Add mini's password to shorthair system
    Before this Minion was unable to sudo on @shorthair, this change fixes
    this.
    
    Change-Id: I2c1f0f568ef9662104be9143ff43165930836438
    Reviewed-on: https://git.clicks.codes/c/Chimera/NixFiles/+/504
    Tested-by: Skyler Grey <minion@clicks.codes>
    Reviewed-by: Skyler Grey <minion@clicks.codes>

--- systems/x86_64-linux/shorthair/users/default.nix
+++ systems/x86_64-linux/shorthair/users/default.nix
@@ -3,6 +3,9 @@
   users.users.coded.hashedPasswordFile =
     config.sops.secrets."systems/x86_64-linux/shorthair/users/passwords.sops.coded.json:coded".path;
 
+  users.users.minion.hashedPasswordFile =
+    config.sops.secrets."systems/x86_64-linux/shorthair/users/passwords.sops.coded.json:minion".path;
+
   sops.secrets."systems/x86_64-linux/shorthair/users/passwords.sops.coded.json:coded" = {
     mode = "0400";
     owner = config.users.users.root.name;
@@ -10,6 +13,16 @@
     sopsFile = ./passwords.sops.coded.json;
     format = "json";
     key = "coded";
+    neededForUsers = true;
+  };
+
+  sops.secrets."systems/x86_64-linux/shorthair/users/passwords.sops.coded.json:minion" = {
+    mode = "0400";
+    owner = config.users.users.root.name;
+    group = config.users.users.root.group;
+    sopsFile = ./passwords.sops.coded.json;
+    format = "json";
+    key = "minion";
     neededForUsers = true;
   };
 }
