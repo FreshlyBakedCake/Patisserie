1bf9e19f969d7da7ec6ec99cbae7eaeb3df3f513
Skyler Grey <minion@clicks.codes>
2024-05-24 23:18:37 +0000

    feat(greylag): limit CPU speed by default
    The vast majority of the time, I don't need my CPU to go as fast as it
    can. Unfortunately, it going as fast as it can will make it too hot so it
    is unable to maintain it for prolonged periods of time.
    
    Limiting it by default will prevent CPU heating while providing no
    appreciable performance impact for regular activity.
    
    Change-Id: I604835ffd4f096f5075b1746db71c69f1e225e70
    Reviewed-on: https://git.clicks.codes/c/Chimera/NixFiles/+/701
    Reviewed-by: Skyler Grey <minion@clicks.codes>
    Tested-by: Skyler Grey <minion@clicks.codes>

--- systems/x86_64-linux/greylag/cpu/default.nix
+++ systems/x86_64-linux/greylag/cpu/default.nix
@@ -1,5 +1,9 @@
 { config, ... }:
 {
-  powerManagement.cpuFreqGovernor = "powersave";
+  powerManagement = {
+    cpuFreqGovernor = "powersave";
+    cpufreq.max = 1500000;
+    # Running at full frequency heats up too much and throttles us, we can do it for a little but we should actively set it iff we need it
+  };
   hardware.cpu.intel.updateMicrocode = config.hardware.enableRedistributableFirmware;
 }
