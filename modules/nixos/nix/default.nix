625cc392d3abb813c4aafd9245a53108b25dbba7
Skyler Grey <minion@clicks.codes>
2024-05-25 13:06:12 +0000

    feat(nix): add more binary caches
    Change-Id: I4b032b661bc8f20d53ac549140e5e5d58195953e
    Reviewed-on: https://git.clicks.codes/c/Chimera/NixFiles/+/712
    Tested-by: Skyler Grey <minion@clicks.codes>
    Reviewed-by: Samuel Shuert <coded@clicks.codes>

--- modules/nixos/nix/default.nix
+++ modules/nixos/nix/default.nix
@@ -6,18 +6,26 @@
     builders-use-substitutes = true;
 
     substituters = [
-      "https://cache.nixos.org"
       "https://anyrun.cachix.org"
+      "https://cache.lix.systems"
+      "https://cache.nixos.org"
+      "https://nix-community.cachix.org"
     ];
 
     trusted-public-keys = [
-      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
       "anyrun.cachix.org-1:pqBobmOjI7nKlsUMV25u9QHa9btJK65/C8vnO3p346s="
+      "cache.lix.systems:aBnZUw8zA7H35Cz2RyKFVs3H4PlGTLawyY5KRbvJR8o="
+      "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
+      "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
     ];
 
     experimental-features = [
       "nix-command"
       "flakes"
+    ];
+
+    trusted-users = [
+      "@wheel"
     ];
   };
 
