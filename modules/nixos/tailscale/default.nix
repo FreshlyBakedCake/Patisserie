8a24c20174043314fad6a03dd85ebdc81e341c65
Skyler Grey <minion@clicks.codes>
2024-06-09 13:51:45 +0000

    feat: add tailscale
    Tailscale is a VPN which allows easily connecting lots of different
    devices together and accessing them via easy-to-remember names. We want
    to use it to make development and server management easier
    
    Change-Id: I76a95258664410ba0e10a08853c646aa54689714
    Reviewed-on: https://git.clicks.codes/c/Chimera/NixFiles/+/738
    Reviewed-by: Samuel Shuert <coded@clicks.codes>
    Tested-by: Skyler Grey <minion@clicks.codes>

---
+++ modules/nixos/tailscale/default.nix
@@ -0,0 +1,43 @@
+{
+  config,
+  lib,
+  pkgs,
+  ...
+}: let
+  cfg = config.chimera.networking.tailscale;
+in {
+  options.chimera.networking.tailscale = {
+    enable = lib.mkOption {
+      description = "Enable tailscale for this system";
+      default = true;
+      type = lib.types.bool;
+    };
+    runExitNode.enable = lib.mkEnableOption "Enable this system as an exit node on the tailnet";
+    server = lib.mkOption {
+      description = "Set where your control plane server is";
+      default = "https://clicks.domains";
+      example = "https://controlplane.tailscale.com";
+    };
+    authKeyFile = lib.mkOption {
+      type = lib.types.nullOr lib.types.str;
+      description = "Path to key file for tailscale";
+      default = null;
+    };
+  };
+
+  config = lib.mkIf cfg.enable {
+    services.tailscale = {
+      enable = true;
+      useRoutingFeatures = if cfg.runExitNode.enable then "server" else "client";
+      extraUpFlags = [
+        "--login-server=${cfg.server}"
+        "--accept-routes"
+        "--ssh"
+      ] ++ (if cfg.runExitNode.enable then [
+        "--advertise-exit-node"
+        "--exit-node-allow-lan-access"
+      ] else []);
+      authKeyFile = lib.mkIf (cfg.authKeyFile != null) cfg.authKeyFile;
+    };
+  };
+}
--- systems/x86_64-linux/greylag/networking/default.nix
+++ systems/x86_64-linux/greylag/networking/default.nix
@@ -1,4 +1,15 @@
-{
+{ config, ... }: {
   networking.hostName = "greylag";
   networking.useDHCP = true;
+
+  chimera.networking.tailscale.authKeyFile = config.sops.secrets."systems/x86_64-linux/greylag/networking/tailscale.sops.minion.json:authkey".path;
+
+  sops.secrets."systems/x86_64-linux/greylag/networking/tailscale.sops.minion.json:authkey" = {
+    mode = "0400";
+    owner = config.users.users.root.name;
+    group = config.users.users.root.group;
+    sopsFile = ./tailscale.sops.minion.json;
+    format = "json";
+    key = "authkey";
+  };
 }
