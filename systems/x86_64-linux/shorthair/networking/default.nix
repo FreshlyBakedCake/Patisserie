fd87435c5e3f965adaa546c942dabf297cccaebe
Samuel Shuert <coded@clicks.codes>
2024-06-14 16:56:03 -0400

    feat(shorthair): enable tailscale
    Change-Id: If0442e92caea7946fec210042af1f09fa01921c6
    Reviewed-on: https://git.clicks.codes/c/Chimera/NixFiles/+/755
    Reviewed-by: Samuel Shuert <coded@clicks.codes>
    Tested-by: Samuel Shuert <coded@clicks.codes>

--- systems/x86_64-linux/shorthair/networking/default.nix
+++ systems/x86_64-linux/shorthair/networking/default.nix
@@ -1,7 +1,18 @@
-{ lib, ... }:
+{ lib, config, ... }:
 {
   networking = {
     hostName = "shorthair";
     useDHCP = lib.mkDefault true;
+  };
+
+  chimera.networking.tailscale.authKeyFile = config.sops.secrets."systems/x86_64-linux/shorthair/networking/tailscale.sops.coded.json:authkey".path;
+
+  sops.secrets."systems/x86_64-linux/shorthair/networking/tailscale.sops.coded.json:authkey" = {
+    mode = "0400";
+    owner = config.users.users.root.name;
+    group = config.users.users.root.group;
+    sopsFile = ./tailscale.sops.coded.json;
+    format = "json";
+    key = "authkey";
   };
 }
