9d53965cf6976b37d37cb478524c6481af1ccd33
Skyler Grey <minion@clicks.codes>
2024-06-30 23:44:06 +0000

    feat(greylag): Add coded
    Coded should be able to ssh to greylag and use sudo commands. To
    facilitate this, let's give him a user
    
    Change-Id: I5e74200ed984c1258e0c6482d32a97c7a426bcc8
    Reviewed-on: https://git.clicks.codes/c/Chimera/NixFiles/+/774
    Tested-by: Skyler Grey <minion@clicks.codes>
    Reviewed-by: Skyler Grey <minion@clicks.codes>

--- systems/x86_64-linux/greylag/users/default.nix
+++ systems/x86_64-linux/greylag/users/default.nix
@@ -2,6 +2,8 @@
 {
   users.users.minion.hashedPasswordFile =
     config.sops.secrets."systems/x86_64-linux/greylag/users/passwords.sops.minion.json:minion".path;
+  users.users.coded.hashedPasswordFile =
+    config.sops.secrets."systems/x86_64-linux/greylag/users/passwords.sops.minion.json:coded".path;
 
   sops.secrets."systems/x86_64-linux/greylag/users/passwords.sops.minion.json:minion" = {
     mode = "0400";
@@ -10,6 +12,15 @@
     sopsFile = ./passwords.sops.minion.json;
     format = "json";
     key = "minion";
+    neededForUsers = true;
+  };
+  sops.secrets."systems/x86_64-linux/greylag/users/passwords.sops.minion.json:coded" = {
+    mode = "0400";
+    owner = config.users.users.root.name;
+    group = config.users.users.root.group;
+    sopsFile = ./passwords.sops.minion.json;
+    format = "json";
+    key = "coded";
     neededForUsers = true;
   };
 }
