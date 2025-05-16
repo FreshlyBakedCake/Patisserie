68aa5876974f1456ef9bb28ca73deb8dbca45cba
Skyler Grey <minion@freshlybakedca.ke>
2025-05-16 08:51:31 +0000

    feat: auto-upgrade to released system
    This is almost the last piece I need to make sure my systems stay
    up-to-date. This will act as an upgrader on the machine itself. I will
    also need a piece like renovate/etc. to make automated upgrading pull
    requests

--- packetmix/systems/redhead/packetmix.nix
+++ packetmix/systems/redhead/packetmix.nix
@@ -1,11 +1,26 @@
 /*
   packetmix.nix: packetmix support configuration, including our binary cache and auto-updating
 */
-{
+{ config, pkgs, ... }: {
   nix.settings.substituters = [
     "https://cache.nixos.org"
   ];
   nix.settings.trusted-public-keys = [
     "cache.nixos.org-1:6NCHdD59X431o0gWypbMrAURkbJ16ZPMQFGspcDShjY="
   ];
+
+  system.autoUpgrade = {
+    enable = true;
+    operation = "boot"; # The default is "switch", but that can lead to some nasty inconsistencies - boot is cleaner
+    flags = [
+      "-f" "/etc/nixos/nilla.nix"
+      "-A" "systems.nixos.${config.networking.hostName}.result"
+    ];
+  };
+
+  systemd.services.nixos-upgrade.preStart = ''
+    cd /etc/nixos
+    ${pkgs.git}/bin/git fetch
+    ${pkgs.git}/bin/git checkout origin/release
+  '';
 }
