55c8fba12e06f632a86cdc6c968b8858619ec4b1
Skyler Grey <minion@freshlybakedca.ke>
2025-05-16 14:08:47 +0000

    feat: add zoxide
    We have previously used zoxide to make our cd command go to commonly
    used directories as well as whatever was specified - therefore, we'll
    override the 'cd' command with it
    
    I've also set it so it updates on prompt, as I find that the more time I
    spend in a directory, the more likely I am to want to cd there

--- packetmix/homes/default.nix
+++ packetmix/homes/default.nix
@@ -14,6 +14,7 @@ in {
       ./minion/direnv.nix
       ./minion/ghostty.nix
       ./minion/ripgrep.nix
+      ./minion/zoxide.nix
     ];
     args = {
       system = "x86_64-linux";
---
+++ packetmix/homes/minion/zoxide.nix
@@ -0,0 +1,10 @@
+# SPDX-FileCopyrightText: 2025 FreshlyBakedCake
+#
+# SPDX-License-Identifier: MIT
+
+{
+  programs.zoxide = {
+    enable = true;
+    options = [ "--cmd=cd" "--hook=prompt" ];
+  };
+}
