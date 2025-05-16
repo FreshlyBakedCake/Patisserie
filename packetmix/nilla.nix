78bf3f8ef06bb453e157e39593342b35f2b9af45
Skyler Grey <minion@freshlybakedca.ke>
2025-05-16 08:51:31 +0000

    build: create NixOS build workflow
    I want to auto-update to my nixos config, but I don't want to build
    everything in a million places! Therefore, I can push to a cache
    and only download from a branch that I know is already cached
    
    I need to build every NixOS system so I can cache them all, the easiest
    way to do this is to just make a package that symlinks them all into
    place
    
    This package will need to be built once for every architecture we
    support. Right now that's only x86_64. If we add home/darwin support we
    will need separate packages for those

--- packetmix/nilla.nix
+++ packetmix/nilla.nix
@@ -12,6 +12,22 @@ in
       # Add Nixpkgs as an input (match the name you used when pinning).
       inputs = builtins.mapAttrs (_: value: { src = value; }) pins;
 
+      packages.allNixOSSystems = {
+        systems = [ "x86_64-linux" ];
+
+        package = { stdenv }: stdenv.mkDerivation {
+          name = "all-nixos-systems";
+
+          dontUnpack = true;
+
+          buildPhase = ''
+            mkdir -p $out
+          '' + (builtins.concatStringsSep "\n" (
+            config.lib.attrs.mapToList (name: value: ''ln -s "${value.result.config.system.build.toplevel}" "$out/${name}"'') config.systems.nixos
+          ));
+        };
+      };
+
       # With a package set defined, we can create a shell.
       shells.default = {
         # Declare what systems the shell can be used on.
