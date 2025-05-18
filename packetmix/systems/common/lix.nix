15ab3615c0e9e711bc65799648f006df96c0088b
Samuel Shuert <me@thecoded.prof>
2025-05-18 18:14:14 -0400

    feat: Add GC and migrate nix experimental features

--- packetmix/systems/common/lix.nix
+++ packetmix/systems/common/lix.nix
@@ -2,4 +2,13 @@
   imports = [
     monorepo.inputs.lix-module.result.nixosModules.default
   ];
+
+  nix.settings.experimental-features = [ "nix-command" ];
+
+  nix.gc = {
+    automatic = true;
+    persistent = true;
+    options = "--delete-older-than 7d";
+    dates = "08:30";
+  };
 }
