8849825cdf378bc5ce84693819ee5df28fc72c4a
Skyler Grey <minion@freshlybakedca.ke>
2025-06-21 10:18:09 +0000

    fix(ssh-agent): add SSH_ASKPASS
    Previously we enabled the ssh-agent for users but didn't add any askpass
    command. This meant that if you were using a key that needed a pin it
    would immediately fail.
    
    We can fix that by providing an SSH_ASKPASS environment variable to
    the service as it is started. Setting this for shells (e.g. via NixOS'
    programs.ssh.enableAskPassword) doesn't work as the agent never gets
    access to the set variable

--- packetmix/homes/remote/ssh.nix
+++ packetmix/homes/remote/ssh.nix
@@ -2,7 +2,10 @@
 #
 # SPDX-License-Identifier: MIT
 
+{ pkgs, ... }:
 {
   services.ssh-agent.enable = true;
+  systemd.user.services.ssh-agent.Service.Environment =
+    "SSH_ASKPASS=${pkgs.kdePackages.ksshaskpass}/bin/ksshaskpass";
   home.sessionVariables.SSH_AUTH_SOCK = "/run/user/$UID/ssh-agent";
 }
