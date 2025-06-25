1902ec2bb884e09785b880e0b015333dc1574304
Skyler Grey <minion@freshlybakedca.ke>
2025-06-25 09:42:56 +0000

    feat: disable espanso notifications
    espanso sends lots of notifications - one for each time it starts and
    one every time the config is edited (which includes each home manager
    activation)
    
    This is a little excessive - let's disable it

--- packetmix/homes/espanso/espanso.nix
+++ packetmix/homes/espanso/espanso.nix
@@ -6,6 +6,7 @@
 {
   xdg.configFile."espanso/config/default.yml".text = builtins.toJSON {
     search_trigger = ":search";
+    show_notifications = false;
     keyboard_layout = {
       model = config.home.keyboard.model;
       layout = config.home.keyboard.layout;
