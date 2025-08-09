fa4219db941c72607730bfd8beee0e752e05b78a
Skyler Grey <minion@freshlybakedca.ke>
2025-08-09 14:30:45 +0000

    feat: move chromium to homes
    Chromium was in our configuration.nix. As part of our ongoing quest
    to move stuff out, I've moved it out. I moved it to homes so that
    impermanence could store user data without knowing the username at the
    system level.

--- packetmix/systems/personal/configuration.nix
+++ packetmix/systems/personal/configuration.nix
@@ -25,7 +25,6 @@
   environment.systemPackages = with pkgs; [
     #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
     #  wget
-    chromium
     dogdns
     ghostty
     (project.inputs.npins.result { inherit pkgs system; })
