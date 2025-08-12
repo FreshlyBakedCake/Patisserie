848d354cc8e1c0fe42f0ef4df94f29ebb4e1a51d
Maya Stephens <maya.stephens@collabora.com>
2025-08-12 16:20:01 +0000

    feat(maya): change catppuccin theme

--- packetmix/homes/maya/catppuccin.nix
+++ packetmix/homes/maya/catppuccin.nix
@@ -1,9 +1,10 @@
+# SPDX-FileCopyrightText: 2025 Collabora Productivity Limited
 # SPDX-FileCopyrightText: 2025 FreshlyBakedCake
 #
 # SPDX-License-Identifier: MIT
 {
   catppuccin = {
-    flavor = "latte";
-    accent = "maroon";
+    flavor = "mocha";
+    accent = "mauve";
   };
 }
