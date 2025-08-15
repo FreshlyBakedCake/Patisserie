cc1bbcf7f8065b5a380d9351c2691a84e446369e
Maya Stephens <maya.stephens@collabora.com>
2025-08-15 17:07:34 +0000

    fix(marbled): enable bolt
    Marbled was failing to use a USB mouse with the docking port; enable bolt to allow the docking port to be authorised.

--- packetmix/systems/marbled/hardware-configuration.nix
+++ packetmix/systems/marbled/hardware-configuration.nix
@@ -65,4 +65,6 @@
 
   nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
   hardware.cpu.intel.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  services.hardware.bolt.enable = true;
 }
