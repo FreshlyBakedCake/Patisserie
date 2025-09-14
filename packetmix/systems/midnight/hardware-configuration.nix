8d1b877fb76da21c1ce93f63f12532af4622938e
Skyler Grey <minion@freshlybakedca.ke>
2025-09-14 00:26:57 +0000

    feat(midnight): update hardware-configuration
    We're reinstalling midnight! Previously we had the old hardware config
    here, but that one didn't support impermanence and was pretty different
    to teal's. As part of our reinstall we've repartitioned our disks to
    have a much more similar layout...

--- packetmix/systems/midnight/hardware-configuration.nix
+++ packetmix/systems/midnight/hardware-configuration.nix
@@ -2,36 +2,39 @@
 #
 # SPDX-License-Identifier: MIT
 
-{ lib, config, ... }:
 {
   boot.initrd.availableKernelModules = [
     "nvme"
     "xhci_pci"
+    "ahci"
     "usbhid"
+    "uas"
     "usb_storage"
     "sd_mod"
   ];
   boot.initrd.kernelModules = [ ];
   boot.kernelModules = [ "kvm-amd" ];
   boot.extraModulePackages = [ ];
-  fileSystems."/" = {
-    device = "/dev/disk/by-uuid/0456a002-1692-4ed0-a233-d6cd76c8c2dd";
+  fileSystems."/nix" = {
+    device = "/dev/disk/by-uuid/34b14ffd-62b4-4bca-a1b5-349a6eb7cb28";
     fsType = "btrfs";
+    options = [ "subvol=@nix" ];
   };
-  boot.initrd.luks.devices."luks-ssd0".device =
-    "/dev/disk/by-uuid/a50b2c75-dd36-4d31-924f-d4b77b94efa9";
   fileSystems."/boot" = {
-    device = "/dev/disk/by-uuid/9416-209A";
+    device = "/dev/disk/by-uuid/0CE9-FAD2";
     fsType = "vfat";
+    options = [
+      "fmask=0022"
+      "dmask=0022"
+    ];
   };
-  swapDevices = [ { device = "/dev/disk/by-uuid/a1cb08ad-39b3-4a36-bf5a-fad7714a85c0"; } ];
-  # Enables DHCP on each ethernet and wireless interface. In case of scripted networking
-  # (the default) this is the recommended approach. When using systemd-networkd it's
-  # still possible to use this option, but it's recommended to use it in conjunction
-  # with explicit per-interface declarations with `networking.interfaces.<interface>.useDHCP`.
-  networking.useDHCP = lib.mkDefault true;
-  # networking.interfaces.enp1s0.useDHCP = lib.mkDefault true;
-  # networking.interfaces.wlo1.useDHCP = lib.mkDefault true;
-  nixpkgs.hostPlatform = lib.mkDefault "x86_64-linux";
-  hardware.cpu.amd.updateMicrocode = lib.mkDefault config.hardware.enableRedistributableFirmware;
+
+  clicks.storage.impermanence = {
+    enable = true;
+    devices = {
+      root = "/dev/disk/by-uuid/34b14ffd-62b4-4bca-a1b5-349a6eb7cb28";
+      persist = "/dev/disk/by-uuid/34b14ffd-62b4-4bca-a1b5-349a6eb7cb28";
+    };
+  };
+  fileSystems."/persist".options = [ "subvol=@persist" ];
 }
