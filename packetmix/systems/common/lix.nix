1bee608b6adbdf268819e7dc6c1c532051faba6a
Skyler Grey <minion@freshlybakedca.ke>
2025-09-18 13:11:04 +0000

    feat: use nix-output-monitor for builds
    It'd be nice to use nix output monitor for builds - there's a project
    which automatically uses nix output monitor when nix is called (even
    supporting nix-direnv/nixos-rebuild)... this looks probably good for us
    to use.
    
    There is [one issue](https://github.com/ners/nix-monitored/issues/3)
    of some importance on the nix-monitored tracker which suggests that
    nix-monitored incorrectly transforms some 'nix run' invocations...
    fortunately, we don't use flakes so this shouldn't be a huge issue
    
    If it is, using
    
        NIX_MONITOR=disable
    
    will temporarily disable nix monitor, allowing you to run whatever you
    needed without issue while using nom for everything else

--- packetmix/systems/common/lix.nix
+++ packetmix/systems/common/lix.nix
@@ -2,7 +2,12 @@
 #
 # SPDX-License-Identifier: MIT
 
-{ project, ... }:
+{
+  project,
+  pkgs,
+  system,
+  ...
+}:
 {
   imports = [
     (import "${project.inputs.lix-module.result}/module.nix" { lix = project.inputs.lix.src; })
@@ -16,4 +21,21 @@
     options = "--delete-older-than 7d";
     dates = "08:30";
   };
+
+  nixpkgs.overlays = [
+    (final: prev: {
+      nix-monitored = project.inputs.nix-monitored.result.packages.${system}.default.override {
+        nix = final.lix;
+        withNotify = pkgs.stdenv.isLinux;
+      };
+      nixos-rebuild = prev.nixos-rebuild.override {
+        nix = final.nix-monitored;
+      };
+      nix-direnv = prev.nix-direnv.override {
+        nix = final.nix-monitored;
+      };
+    })
+  ];
+
+  nix.package = pkgs.nix-monitored;
 }
