97a21bf78d15f8ee86f8f7516759f04434983918
Skyler Grey <minion@freshlybakedca.ke>
2026-01-03 16:41:17 +0000

    feat(m): package for nix
    If we want to use menu on teal, it needs to be packaged for Nix. Let's
    do that!

--- menu/project.nix
+++ menu/project.nix
@@ -5,6 +5,41 @@
 { config, lib }:
 {
   config = {
+    packages.default = config.packages.menu;
+    packages.menu = {
+      systems = [
+        "x86_64-linux"
+        "aarch64-linux"
+      ];
+
+      package =
+        {
+          fenix,
+          makeRustPlatform,
+          lib,
+          ...
+        }:
+        let
+          toolchain = fenix.complete.toolchain;
+
+          manifest = (lib.importTOML ./Cargo.toml).package;
+
+          platform = makeRustPlatform {
+            cargo = toolchain;
+            rustc = toolchain;
+          };
+        in
+        platform.buildRustPackage {
+          meta.mainProgram = "menu";
+          pname = manifest.name;
+          version = manifest.version;
+
+          src = ./.;
+
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+    };
+
     shells.default = config.shells.menu;
     shells.menu = {
       systems = [ "x86_64-linux" ];
