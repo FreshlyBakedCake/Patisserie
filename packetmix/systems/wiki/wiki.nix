b0078362a8f7583963ea23b4838a7f92f7e5143a
Skyler Grey <minion@freshlybakedca.ke>
2026-01-06 20:25:51 +0000

    feat(pm/wiki): enable autosaving
    If we edit the wiki but don't save, we'll lose our edits. We can enable
    autosaving to let users use a setting to ... let that not happen! So
    let's let that not happen!

--- packetmix/systems/wiki/wiki.nix
+++ packetmix/systems/wiki/wiki.nix
@@ -265,6 +265,8 @@
         $wgMFEnableMobilePreferences = true;
         wfLoadSkin( 'MinervaNeue' );
 
+        $wgEnableEditRecovery = true;
+
         $wgShowExceptionDetails = true;
         $wgDevelopmentWarnings = true;
       '';
