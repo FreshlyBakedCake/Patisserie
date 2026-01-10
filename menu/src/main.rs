9fdd95f1de98c21099650b963565702989f1e1bd
Skyler Grey <minion@freshlybakedca.ke>
2026-01-10 12:27:02 +0000

    feat(m): add better UI for creating shortcuts
    I've made a few pages to create and delete shortcuts, as well as handle
    all of the error and confirmation cases that come out of that.
    
    The templating is rather nasty - I believe it's *safe* but I'm not
    certain that it's completely *correct* (i.e. that there are no cases
    which mangle a value somewhere). Additionally, I'm unhappy with the
    general string rewriting garbage this leads to... there's nothing to
    stop, say, double-templating/etc. Probably we need to at minimum do a
    regex replace with a function to fetch the replacements properly, but
    I think we may be reaching the point where switching to something other
    than plain HTML is worth it...
    
    We use the execCommand method for copying rather than
    navigator.clipboard because one intention of this is to be accessible at
    http://go/ - which isn't a secure context. If that's the case, we can't
    copy with navigator.clipboard, but execCommand("copy") will still work.
    Yay for web APIs!
    
    Finally, we need UI for listing out shortcuts. That should probably
    be next, over even implementing regex shortcuts (despite I really want
    to...)

--- menu/src/main.rs
+++ menu/src/main.rs
@@ -1,34 +1,76 @@
 // SPDX-FileCopyrightText: 2026 Freshly Baked Cake
 //
 // SPDX-License-Identifier: MIT
-
 use axum::{
-    Form, Router, ServiceExt,
+    Router, ServiceExt,
     body::Body,
     extract::{Path, Query, Request},
     http::{HeaderMap, Response, StatusCode},
-    response::{Html, IntoResponse, Redirect},
-    routing::{get, post},
+    response::{ErrorResponse, Html, IntoResponse, Redirect, Result},
+    routing::get,
 };
-use serde::Deserialize;
+use include_dir::{Dir, include_dir};
+use percent_encoding::{NON_ALPHANUMERIC, utf8_percent_encode};
 use sqlx::{Connection, PgConnection};
+
+#[cfg(debug_assertions)]
+use std::fs;
+
 use std::{collections::HashMap, env, ops::DerefMut, sync::OnceLock};
 use tokio::{
     sync::Mutex,
     time::{Duration, sleep},
 };
-use tower_http::normalize_path::NormalizePathLayer;
+use tower_http::{self, normalize_path::NormalizePathLayer};
 use tower_layer::Layer;
+use tower_serve_static;
 
-fn template_html(html: &str, replacements: &HashMap<&str, Option<&str>>) -> String {
-    let mut result = html.to_owned();
-    for (&text, &maybe_replacement) in replacements {
-        if let Some(replacement) = maybe_replacement {
-            result = result.replace(&("{".to_owned() + text + "}"), replacement)
-        }
+static PUBLIC_DIR: Dir<'static> = include_dir!("src/html/public");
+
+#[cfg(debug_assertions)]
+static DEVELOPMENT: OnceLock<bool> = OnceLock::new();
+
+fn template_html<'a>(
+    mut html: String,
+    replacements: HashMap<&str, Box<dyn 'a + FnOnce() -> Option<&'a str>>>,
+) -> String {
+    for (text, maybe_replacement) in replacements {
+        let replacement = maybe_replacement().unwrap_or_else(|| "");
+        html = html.replace(
+            &("{".to_owned() + text + "}"),
+            &html_escape::encode_text(replacement),
+        );
+        html = html.replace(
+            &("{".to_owned() + text + ":attribute}"),
+            &html_escape::encode_quoted_attribute(replacement),
+        );
+        html = html.replace(
+            &("{".to_owned() + text + ":url}"),
+            &utf8_percent_encode(replacement, NON_ALPHANUMERIC).to_string(),
+        );
     }
 
-    result
+    html
+}
+
+/// include_str, but if DEVELOPMENT then the string is dynamically fetched for easy reloading
+/// to support this, the string is *always* owned.
+#[cfg(debug_assertions)]
+macro_rules! include_String_dynamic {
+    ($file:expr $(,)?) => {
+        if (*DEVELOPMENT.get().unwrap()) {
+            fs::read_to_string("src/".to_string() + $file)
+                .expect(format!("Unable to read file {}", $file).as_str())
+        } else {
+            include_str!($file).to_owned()
+        }
+    };
+}
+#[cfg(not(debug_assertions))]
+macro_rules! include_String_dynamic {
+    ($file:expr $(,)?) => {
+        include_str!($file).to_owned()
+    };
 }
 
 #[derive(Debug)]
@@ -76,15 +118,23 @@ async fn get_redirect(default_location: &str, go: &str) -> Redirect {
 }
 
 async fn get_redirect_base(go: &str) -> Redirect {
-    get_redirect("/_/create?path=", go).await
+    get_redirect("/_/create?from=", go).await
 }
 
 async fn get_redirect_search(go: &str) -> Redirect {
     get_redirect("https://kagi.com/search?q=", go).await
 }
 
-async fn handle_root() -> String {
-    "Hello, world!".to_string()
+async fn handle_root(
+    headers: HeaderMap,
+    #[cfg(debug_assertions)] Query(params): Query<HashMap<String, String>>,
+) -> Result<String> {
+    ensure_authenticated(
+        &headers,
+        #[cfg(debug_assertions)]
+        &params,
+    )?;
+    Ok("Hello, world!".to_string())
 }
 
 async fn handle_base(Path(go): Path<String>) -> Redirect {
@@ -99,50 +149,236 @@ async fn handle_search(Query(params): Query<HashMap<String, String>>) -> Redirec
     }
 }
 
+#[derive(Clone)]
+enum StaticPageType {
+    Create,
+    CreateSuccess,
+    CreateConflict,
+    CreateFailure,
+    DeleteSuccess,
+    DeleteFailure,
+}
+
+async fn handle_static_page<'a>(
+    page_type: StaticPageType,
+    params: &'a HashMap<String, String>,
+    headers: &'a HeaderMap,
+) -> Result<Html<String>> {
+    let auth_required = match page_type {
+        _ => true,
+    };
+
+    let html = match page_type {
+        StaticPageType::Create => include_String_dynamic!("./html/create.html"),
+        StaticPageType::CreateSuccess => include_String_dynamic!("./html/create/success.html"),
+        StaticPageType::CreateConflict => include_String_dynamic!("./html/create/conflict.html"),
+        StaticPageType::CreateFailure => include_String_dynamic!("./html/create/failure.html"),
+        StaticPageType::DeleteSuccess => include_String_dynamic!("./html/delete/success.html"),
+        StaticPageType::DeleteFailure => include_String_dynamic!("./html/delete/failure.html"),
+    };
+
+    let username = if auth_required {
+        Some(ensure_authenticated(
+            headers,
+            #[cfg(debug_assertions)]
+            params,
+        )?)
+    } else {
+        None
+    };
+
+    let mut replacements: HashMap<&str, Box<dyn 'a + FnOnce() -> Option<&'a str>>> = HashMap::new();
+    replacements.insert(
+        "host",
+        Box::new(|| {
+            Some(clean_host(
+                headers
+                    .get("host")
+                    .and_then(|header| Some(header.to_str().unwrap_or_else(|_| "go")))
+                    .unwrap_or_else(|| "go"),
+            ))
+        }),
+    );
+    replacements.insert(
+        "from",
+        Box::new(|| params.get("from").and_then(|from| Some(from.as_str()))),
+    );
+    replacements.insert(
+        "to",
+        Box::new(|| params.get("to").and_then(|to| Some(to.as_str()))),
+    );
+    replacements.insert(
+        "current",
+        Box::new(|| {
+            params
+                .get("current")
+                .and_then(|current| Some(current.as_str()))
+        }),
+    );
+    replacements.insert("username", Box::new(move || username));
+
+    let result = template_html(html, replacements);
+    Ok(Html(result))
+}
+
 async fn handle_create_page(
     Query(params): Query<HashMap<String, String>>,
     headers: HeaderMap,
-) -> Html<String> {
-    Html(template_html(
-        include_str!("./html/create.html"),
-        &HashMap::from([
-            (
-                "host",
-                Some(clean_host(
-                    headers
-                        .get("host")
-                        .and_then(|header| Some(header.to_str().unwrap_or_else(|_| "go")))
-                        .unwrap_or_else(|| "go"),
-                )),
-            ),
-            (
-                "path",
-                params.get("path").and_then(|path| Some(path.as_str())),
-            ),
-        ]),
-    ))
+) -> Result<Html<String>> {
+    handle_static_page(StaticPageType::Create, &params, &headers).await
+}
+async fn handle_create_success_page(
+    Query(params): Query<HashMap<String, String>>,
+    headers: HeaderMap,
+) -> Result<Html<String>> {
+    handle_static_page(StaticPageType::CreateSuccess, &params, &headers).await
+}
+async fn handle_create_conflict_page(
+    Query(params): Query<HashMap<String, String>>,
+    headers: HeaderMap,
+) -> Result<Html<String>> {
+    handle_static_page(StaticPageType::CreateConflict, &params, &headers).await
+}
+async fn handle_create_failure_page(
+    Query(params): Query<HashMap<String, String>>,
+    headers: HeaderMap,
+) -> Result<impl IntoResponse> {
+    handle_static_page(StaticPageType::CreateFailure, &params, &headers)
+        .await
+        .and_then(|html| Ok((StatusCode::INTERNAL_SERVER_ERROR, html)))
+}
+async fn handle_delete_success_page(
+    Query(params): Query<HashMap<String, String>>,
+    headers: HeaderMap,
+) -> Result<Html<String>> {
+    handle_static_page(StaticPageType::DeleteSuccess, &params, &headers).await
+}
+async fn handle_delete_failure_page(
+    Query(params): Query<HashMap<String, String>>,
+    headers: HeaderMap,
+) -> Result<Html<String>> {
+    handle_static_page(StaticPageType::DeleteFailure, &params, &headers).await
 }
 
-#[derive(Deserialize)]
-struct Create {
-    from: String,
-    to: String,
+struct NotAuthenticated;
+impl IntoResponse for NotAuthenticated {
+    fn into_response(self) -> axum::response::Response {
+        return (StatusCode::UNAUTHORIZED, "Access over Tailscale only").into_response();
+    }
+}
+
+fn ensure_authenticated<'a>(
+    headers: &'a HeaderMap,
+    #[cfg(debug_assertions)] params: &'a HashMap<String, String>,
+) -> Result<&'a str, ErrorResponse> {
+    if let Some(user) = headers
+        .get("X-Webauth-Login")
+        .and_then(|header| header.to_str().ok())
+    {
+        return Ok(user);
+    }
+
+    #[cfg(debug_assertions)]
+    {
+        if *DEVELOPMENT.get().unwrap() {
+            if let Some(user) = params.get("dev_auth_as") {
+                return Ok(user);
+            }
+        }
+    }
+
+    Err(NotAuthenticated {}.into())
 }
 
 #[axum::debug_handler]
-async fn handle_create_post(headers: HeaderMap, Form(create): Form<Create>) -> Response<Body> {
-    let Some(Ok(owner)) = headers
-        .get("X-Webauth-Login")
-        .and_then(|header| Some(header.to_str()))
-    else {
-        return (StatusCode::UNAUTHORIZED, "Access over Tailscale only").into_response();
-    };
+async fn handle_create_do(
+    headers: HeaderMap,
+    Query(params): Query<HashMap<String, String>>,
+) -> Result<Response<Body>> {
+    let owner = ensure_authenticated(
+        &headers,
+        #[cfg(debug_assertions)]
+        &params,
+    )?;
+
+    let from = params.get("from").ok_or("Missing from query")?;
+    let to = params.get("to").ok_or("Missing to query")?;
+
+    println!("Attempting to make go/{} -> {}", from, to);
 
     let create_call = sqlx::query!(
-        r#"INSERT INTO direct ("from", "to", "owner") VALUES ($1, $2, $3) ON CONFLICT ("from") DO UPDATE SET "to" = EXCLUDED.to, "owner" = EXCLUDED.owner"#,
-        create.from.to_lowercase(),
-        create.to,
+        r#"
+        WITH insertion AS (
+            INSERT INTO direct ("from", "to", "owner")
+                VALUES ($1, $2, $3)
+                ON CONFLICT ("from")
+                DO UPDATE SET "to" = EXCLUDED.to, "owner" = EXCLUDED.owner WHERE direct.to = $4
+                RETURNING direct.from
+        )
+        SELECT direct.to FROM direct
+        WHERE direct.from NOT IN (SELECT insertion.from FROM insertion) AND direct.from = $1
+        "#, // Insert our URL, return a row with the same from that weren't updated (i.e. a conflict)
+        from.to_lowercase(),
+        to,
         owner,
+        params.get("current"),
+    )
+    .fetch_optional(
+        STATE
+            .get()
+            .expect("Server must be initialized before processing connections")
+            .sqlx_connection
+            .lock()
+            .await
+            .deref_mut(),
+    )
+    .await;
+
+    if let Ok(None) = &create_call {
+        Ok(Redirect::to(&format!(
+            "/_/create/success?from={}&to={}",
+            utf8_percent_encode(&from, NON_ALPHANUMERIC).to_string(),
+            utf8_percent_encode(&to, NON_ALPHANUMERIC).to_string(),
+        ))
+        .into_response())
+    } else if let Ok(Some(conflict)) = create_call {
+        Ok(Redirect::to(&format!(
+            "/_/create/conflict?from={}&to={}&current={}",
+            utf8_percent_encode(&from, NON_ALPHANUMERIC).to_string(),
+            utf8_percent_encode(&to, NON_ALPHANUMERIC).to_string(),
+            utf8_percent_encode(&conflict.to, NON_ALPHANUMERIC).to_string(),
+        ))
+        .into_response())
+    } else {
+        Ok(Redirect::to(&format!(
+            "/_/create/failure?from={}&to={}",
+            utf8_percent_encode(&from, NON_ALPHANUMERIC).to_string(),
+            utf8_percent_encode(&to, NON_ALPHANUMERIC).to_string(),
+        ))
+        .into_response())
+    }
+}
+
+#[axum::debug_handler]
+async fn handle_delete_do(
+    headers: HeaderMap,
+    Query(params): Query<HashMap<String, String>>,
+) -> Result<Response<Body>> {
+    ensure_authenticated(
+        &headers,
+        #[cfg(debug_assertions)]
+        &params,
+    )?;
+
+    let from = params.get("from").ok_or("Missing from query")?;
+    let current = params.get("current").ok_or("Missing current query")?;
+
+    println!("Attempting to delete go/{} -> {}", from, current);
+
+    let delete_call = sqlx::query!(
+        r#"DELETE FROM direct WHERE direct.from = $1 AND direct.to = $2"#,
+        from.to_lowercase(),
+        current,
     )
     .execute(
         STATE
@@ -155,10 +391,22 @@ async fn handle_create_post(headers: HeaderMap, Form(create): Form<Create>) -> R
     )
     .await;
 
-    if create_call.is_ok() {
-        Redirect::to(&format!("/_/created?path={}", create.from)).into_response()
+    if let Ok(delete_result) = &delete_call
+        && delete_result.rows_affected() > 0
+    {
+        Ok(Redirect::to(&format!(
+            "/_/delete/success?from={}&current={}",
+            utf8_percent_encode(&from, NON_ALPHANUMERIC).to_string(),
+            utf8_percent_encode(&current, NON_ALPHANUMERIC).to_string(),
+        ))
+        .into_response())
     } else {
-        Redirect::to(&format!("/_/createFailed?path={}", create.from)).into_response()
+        Ok(Redirect::to(&format!(
+            "/_/delete/failure?from={}&to={}",
+            utf8_percent_encode(&from, NON_ALPHANUMERIC).to_string(),
+            utf8_percent_encode(&current, NON_ALPHANUMERIC).to_string(),
+        ))
+        .into_response())
     }
 }
 
@@ -168,6 +416,12 @@ async fn handle_404() -> impl IntoResponse {
 
 #[tokio::main]
 async fn main() {
+    #[cfg(debug_assertions)]
+    {
+        DEVELOPMENT
+            .set(env::var("DEVELOPMENT").is_ok_and(|value| value == "true"))
+            .unwrap();
+    }
     let mut connection = {
         let mut maybe_connection;
         let mut tries = 3;
@@ -205,10 +459,36 @@ async fn main() {
         })
         .expect("Consistency issue: failed to set STATE - was it already set?");
 
-    let router = Router::new()
+    let mut router = Router::new();
+
+    #[cfg(not(debug_assertions))]
+    {
+        router = router.nest_service("/_/public", tower_serve_static::ServeDir::new(&PUBLIC_DIR));
+    }
+
+    #[cfg(debug_assertions)]
+    {
+        if *DEVELOPMENT.get().unwrap() {
+            router = router.nest_service(
+                "/_/public",
+                tower_http::services::ServeDir::new("src/html/public"),
+            );
+        } else {
+            router =
+                router.nest_service("/_/public", tower_serve_static::ServeDir::new(&PUBLIC_DIR));
+        }
+    }
+
+    router = router
         .route("/", get(handle_root))
         .route("/_/create", get(handle_create_page))
-        .route("/_/create", post(handle_create_post))
+        .route("/_/create/success", get(handle_create_success_page))
+        .route("/_/create/conflict", get(handle_create_conflict_page))
+        .route("/_/create/failure", get(handle_create_failure_page))
+        .route("/_/create/do", get(handle_create_do))
+        .route("/_/delete/do", get(handle_delete_do))
+        .route("/_/delete/success", get(handle_delete_success_page))
+        .route("/_/delete/failure", get(handle_delete_failure_page))
         .route("/_/search", get(handle_search))
         .route("/_/{*route}", get(handle_404))
         .route("/{*go}", get(handle_base));
