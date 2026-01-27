5a2d50015e7ec7ae60ef943a0647ad5309cce9c2
Skyler Grey <minion@freshlybakedca.ke>
2026-01-27 10:09:28 +0000

    feat(pm/pds): automatically age-assure users
    As a PDS, we make sure our users are over the age of majority. It's
    silly for them to have to also give their ID to KWS to verify their
    age again. We can make the endpoint return The Right Thing to skip out
    on this

--- packetmix/systems/teal/pds.nix
+++ packetmix/systems/teal/pds.nix
@@ -36,6 +36,28 @@
       proxyWebsockets = true;
     };
 
+    locations."/xrpc/app.bsky.ageassurance.getState" = {
+      extraConfig = ''
+        default_type application/json;
+        add_header access-control-allow-headers "authorization,dpop,atproto-accept-labelers,atproto-proxy" always;
+        add_header access-control-allow-origin "*" always;
+        add_header X-Frame-Options SAMEORIGIN always;
+        add_header X-Content-Type-Options nosniff;
+      '';
+      return = "200 '${
+        builtins.toJSON {
+          state = {
+            lastInitiatedAt = "2025-09-11T19:18:03.551Z";
+            status = "assured";
+            access = "full";
+          };
+          metadata.accountCreatedAt = "2023-12-12T20:16:56.499Z";
+        }
+      }'";
+      # Our PDS is private
+      # Therefore, we have verified the age of everyone on the service is over the age of majority - and we didn't need KWS to do it!
+    };
+
     extraConfig = ''
       client_max_body_size 1024M;
     '';
