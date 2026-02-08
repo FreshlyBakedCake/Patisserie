3e29191f1a1ca0128ce4d29e62a93c48c85c29c6
Skyler Grey <minion@freshlybakedca.ke>
2026-02-08 13:30:23 +0000

    feat(pm)!: switch bluesky pds to tranquil PDS
    Tranquil PDS has features we prefer to bluesky's, such as OIDC on
    accounts. Let's switch over!
    
    BREAKING-CHANGE: You are required to manually migrate accounts to the new PDS. Not doing this *will* cause data loss.

--- packetmix/systems/teal/pds.nix
+++ packetmix/systems/teal/pds.nix
@@ -1,67 +1,98 @@
-# SPDX-FileCopyrightText: 2025 FreshlyBakedCake
+# SPDX-FileCopyrightText: 2026 FreshlyBakedCake
 #
 # SPDX-License-Identifier: MIT
 
 {
   project,
+  config,
   system,
+  pkgs,
+  lib,
   ...
 }:
 {
-  services.bluesky-pds = {
+  imports = [ project.inputs.tranquil-pds.result.nixosModules.default ];
+
+  services.tranquil-pds = {
     enable = true;
-    package = project.packages.packetmix-bluesky-pds.result.${system};
-    settings = {
-      PDS_HOSTNAME = "pds.freshly.space";
-      PDS_PORT = 1033;
-      PDS_SERVICE_HANDLE_DOMAINS = ".at.freshlybakedca.ke";
-      PDS_EMAIL_FROM_ADDRESS = "pds@freshly.space";
-      PDS_BLOB_UPLOAD_LIMIT = "268435456";
-    };
     environmentFiles = [
       "/secrets/pds/environmentFile"
+    ];
+
+    database.createLocally = true;
+
+    nginx.enable = true;
+
+    settings = {
+      SERVER_HOST = "127.0.0.1";
+      SERVER_PORT = 1039;
+
+      PDS_HOSTNAME = "at.freshly.space";
+
+      MAIL_FROM_NAME = "Freshly PDS";
+      MAIL_FROM_ADDRESS = "pds@freshly.space";
+      SENDMAIL_PATH = "${pkgs.msmtp}/bin/sendmail";
+
+      VALKEY_URL = "unix://${config.services.redis.servers.tranquil-pds.unixSocket}";
+
+      BACKUP_ENABLED = "true";
+
+      ACCEPTING_REPO_IMPORTS = "true";
+
+      INVITE_CODE_REQUIRED = "true";
+      AVAILABLE_USER_DOMAINS = "at.freshly.space,at.freshlybakedca.ke";
+      ENABLE_SELF_HOSTED_DID_WEB = "false";
+
+      PDS_AGE_ASSURANCE_OVERRIDE = "true";
+      # Our PDS is private
+      # Therefore, we have verified the age of everyone on the service is over the age of majority - and we didn't need KWS to do it!
+
+      SSO_OIDC_ENABLED = "true";
+      SSO_OIDC_CLIENT_ID = "pds";
+      SSO_OIDC_ISSUER = "https://idm.freshly.space/oauth2/openid/pds/";
+      SSO_OIDC_NAME = "Freshly IDM";
+    };
+  };
+
+  services.redis.package = pkgs.valkey;
+  services.redis.servers.tranquil-pds = {
+    enable = true;
+    appendOnly = true;
+    user = "tranquil-pds";
+  };
+
+  systemd.services.tranquil-pds = {
+    wants = [ "redis-tranquil-pds.service" ];
+    after = [ "redis-tranquil-pds.service" ];
+
+    environment = {
+      XDG_CONFIG_HOME = "/secrets/pds";
+    };
+
+    serviceConfig.StateDirectory = [
+      "tranquil-pds/blobs"
+      "tranquil-pds/backups"
+    ]; # Not created automatically by tranquil for some reason...
+  };
+
+  services.nginx.virtualHosts."at.freshly.space" = {
+    acmeRoot = null;
+
+    serverAliases = lib.mkForce [
+      "*.at.freshlybakedca.ke"
+      "*.at.freshly.space"
     ];
   };
 
   services.nginx.virtualHosts."pds.freshly.space" = {
-    addSSL = true;
     enableACME = true;
     acmeRoot = null;
-
-    serverAliases = [ "*.at.freshlybakedca.ke" ];
+    addSSL = true;
 
     locations."/" = {
-      proxyPass = "http://127.0.0.1:1033";
-      recommendedProxySettings = true;
-      proxyWebsockets = true;
+      return = "308 https://at.freshly.space$request_uri";
     };
-
-    locations."/xrpc/app.bsky.ageassurance.getState" = {
-      extraConfig = ''
-        default_type application/json;
-        add_header access-control-allow-headers "authorization,dpop,atproto-accept-labelers,atproto-proxy" always;
-        add_header access-control-allow-origin "*" always;
-        add_header X-Frame-Options SAMEORIGIN always;
-        add_header X-Content-Type-Options nosniff;
-      '';
-      return = "200 '${
-        builtins.toJSON {
-          state = {
-            lastInitiatedAt = "2025-09-11T19:18:03.551Z";
-            status = "assured";
-            access = "full";
-          };
-          metadata.accountCreatedAt = "2023-12-12T20:16:56.499Z";
-        }
-      }'";
-      # Our PDS is private
-      # Therefore, we have verified the age of everyone on the service is over the age of majority - and we didn't need KWS to do it!
-    };
-
-    extraConfig = ''
-      client_max_body_size 1024M;
-    '';
   };
 
-  clicks.storage.impermanence.persist.directories = [ "/var/lib/pds" ];
+  clicks.storage.impermanence.persist.directories = [ "/var/lib/tranquil-pds" ];
 }
