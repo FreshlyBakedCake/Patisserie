d744dc7c14854e72a094c83651b94075ead5f098
Skyler Grey <minion@freshlybakedca.ke>
2026-03-07 10:41:06 +0000

    feat(pm/vicinae): persist config
    Vicinae has some imperative configuration - e.g. whether it should index
    files, etc. - if we don't persist that then we can't change it across
    reboots

--- packetmix/homes/niri/vicinae.nix
+++ packetmix/homes/niri/vicinae.nix
@@ -103,4 +103,8 @@
   systemd.user.services.vicinae.Unit.After = [ "niri.service" ];
   systemd.user.services.vicinae.Install.WandedBy = lib.mkForce [ "niri.service" ];
   systemd.user.services.vicinae.Unit.PartOf = lib.mkForce [ ];
+
+  clicks.storage.impermanence.persist.directories = [
+    ".local/share/vicinae"
+  ];
 }
