92d7a72e9a5449f24bdff9b80ee32b88122f80c6
Skyler Grey <minion@freshlybakedca.ke>
2026-04-05 13:04:43 +0000

    fix(pm/kanidm): update client to 1.9
    When we updated kanidm, we only updated the server ... we need to update
    the client separately :)

--- packetmix/systems/freshlybakedcake+personal/kanidm.nix
+++ packetmix/systems/freshlybakedcake+personal/kanidm.nix
@@ -7,7 +7,7 @@
   services.kanidm = {
     enableClient = true;
 
-    package = lib.lowPrio pkgs.kanidm_1_8; # lowPrio because otherwise `orca` ("Orca Load Testing Utility") from kanidm overrides `orca` the screen reader...
+    package = lib.lowPrio pkgs.kanidm_1_9; # lowPrio because otherwise `orca` ("Orca Load Testing Utility") from kanidm overrides `orca` the screen reader...
 
     clientSettings.uri = "https://idm.freshly.space";
   };
