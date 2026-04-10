5fc658bf285b4aa9e9e9b810080c2daefe132c4d
Skyler Grey <minion@freshlybakedca.ke>
2026-04-10 18:44:49 +0000

    feat(pm/minion): install Signal
    I'd like to use the Signal desktop app - let's install that!

--- packetmix/homes/minion/misc.nix
+++ packetmix/homes/minion/misc.nix
@@ -13,11 +13,13 @@
   # Don't place any config that isn't directly adding lines to home.packages or clicks.storage.impermanence.persist.directories here...
   home.packages = [
     pkgs.obs-studio
+    pkgs.signal-desktop
     pkgs.unzip
     pkgs.zip
   ];
 
   clicks.storage.impermanence.persist.directories = [
     ".config/obs-studio"
+    ".config/Signal"
   ];
 }
