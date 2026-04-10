b62ff5975a8a609b7014c92e9a6060c535a517cc
Skyler Grey <minion@freshlybakedca.ke>
2026-04-10 20:19:15 +0000

    feat(pm/fish): change motd
    So, the original motd for fish is ... fine. But we can add more golinks
    and that's kinda cool!

--- packetmix/homes/minion/fish.nix
+++ packetmix/homes/minion/fish.nix
@@ -5,6 +5,18 @@
 {
   programs.fish.enable = true;
 
+  programs.fish.interactiveShellInit = ''
+    function fish_greeting
+      echo ''
+  # This will be auto-wrapped when it's printed by fish - but it's easier to read in the config like this...
+  + "Welcome to (set_color yellow)fish(set_color normal) on (set_color purple)packetmix(set_color normal) - "
+  + "for help, see (set_color green)go/fishhelp(set_color normal), "
+  + "for configuration see (set_color green)go/packetmix(set_color normal), "
+  + ''
+    for hosted services see (set_color green)go/services(set_color normal)
+        end
+  '';
+
   clicks.storage.impermanence.persist.directories = [
     ".config/fish"
     ".local/share/fish"
