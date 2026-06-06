1e1e1a11ef0d1fa23992592c871973dc84f42577
Skyler Grey <minion@freshlybakedca.ke>
2026-06-06 15:08:02 +0200

    fix(pm/steam): black window on niri
    I'm getting a black window on Steam again on my laptop, this fix seems
    to do the trick:
    
    https://github.com/Supreeeme/xwayland-satellite/issues/150#issuecomment-2847677630

--- packetmix/systems/gaming/steam.nix
+++ packetmix/systems/gaming/steam.nix
@@ -2,9 +2,14 @@
 #
 # SPDX-License-Identifier: MIT
 
+{ pkgs, ... }:
 {
   programs.steam = {
     enable = true;
     remotePlay.openFirewall = true;
+
+    package = pkgs.steam.override {
+      extraArgs = "-system-composer";
+    };
   };
 }
